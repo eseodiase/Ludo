aac6d5988c62c444341a6fa342bed4f93a01df74
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-09 00:08:09 +0100

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
 import React from "react";
 const Home = () => {
-    return <h1>Home page of Lyrics App.</h1>;
+     return <section>
+   <div className="search">
+      <form>
+      <h3 id="title">247 Lyrics Finder</h3>
+      <label>Song name:</label>
+      <input type="search" id="search-song" value="hello">
+      <br/>
+      <label>Artist name:</label>
+    <input type="search" id="search-artist" value="adele">
+    <p className="key"> Hit "Enter key" or tap "Search icon" on your keyboard/keypad to search.</p>
+    </form>
+    </div>
+
+<section className="keyboard">
+    <div className="iframeContainer">
+    <h3>Song Lyrics</h3>
+    <p id="lyrics">Loading...</p>
+    </div>
+    </section>
+
+</section>;
     };
 
     export default Home;
