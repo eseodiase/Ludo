5deb130021f43fdf25df8cf53e865396c04dbd0e
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-09 03:04:27 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -20,45 +20,6 @@ export default function App() {
   </BrowserRouter>
   );
 
-
-
-function d(c){
-          return document.querySelector(c);
-      }
-
-		window.onload = function(){
-		    
- async function fetchData(song, artist){
-       const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1772c9ca1emshf15e2a4b58652a7p1b97c5jsnab1460cddb87',
-		'X-RapidAPI-Host': 'lyrics-plus.p.rapidapi.com'
-	}
-};
-
-fetch(`https://lyrics-plus.p.rapidapi.com/lyrics/${song}/${artist}`, options)
-
-	.then(response => response.json())
-    .then(response => d("#lyrics").innerHTML = (response.name, response.lyrics))
-	.catch(err => console.error(err));
-	
-    
- }    
-const inputRef = d("#search-song") 
-const inputRef2 = d("#search-artist") 
-		 
-  fetchData("hello", "adele")
-	
-inputRef2.addEventListener("search", () =>{
-	fetchData(inputRef.value, inputRef2.value)
-		    
-		 })
-
-		}
-
-
-
   }
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
