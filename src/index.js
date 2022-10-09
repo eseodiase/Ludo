51b6b74e29183580d212b4ce4348fbb8e89265dc
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-10 00:49:25 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -19,42 +19,7 @@ export default function App() {
   </Routes>
   </BrowserRouter>
   );
-
-
-function d(c){
-          return document.querySelector(c);
-      }
-
-		window.onLoad = function(){
-		    
- async function fetchData(song, artist){
-       const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1772c9ca1emshf15e2a4b58652a7p1b97c5jsnab1460cddb87',
-		'X-RapidAPI-Host': 'lyrics-plus.p.rapidapi.com'
-	}
-};
-
-fetch(`https://lyrics-plus.p.rapidapi.com/lyrics/${song}/${artist}`, options)
-
-	.then(response => response.json())
-    .then(response => d("#lyrics").innerHTML = (response.name, response.lyrics))
-	.catch(err => console.error(err));
 	
-    
- }    
-const inputRef = d("#search-song") 
-const inputRef2 = d("#search-artist") 
-		 
-  fetchData("hello", "adele")
-	
-inputRef2.addEventListener("search", () =>{
-	fetchData(inputRef.value, inputRef2.value)
-		    
-		 })
-
-		}
 
   }
 
