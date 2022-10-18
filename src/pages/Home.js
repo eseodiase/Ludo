8aa5d0c7f3d232e280862750d79f9f8cede762f7
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-19 00:28:33 +0100

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,253 @@ const Home = () => {
      return <section className="Hpage">
 <h1> Ludo Board </h1>
 
+<div className="grid-container">
+  <div id="item1" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="white" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="item5" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="item1" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="red" className="item3"></div>  
+  
+  <div id="red"></div>
+  
+  <div id="item5" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="item1" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="red" className="item3"></div>  
+  
+  <div id="white"></div>
+  
+  <div id="item5" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="item1" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="red" className="item3"></div>  
+  
+  <div id="white"></div>
+  
+  <div id="item5" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="item1" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="red" className="item3"></div>  
+  
+  <div id="white"></div>
+  
+  <div id="item5" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="item1" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="red" className="item3"></div>  
+  
+  <div id="white"></div>
+  
+  <div id="item5" className="item5"></div>
+</div>
+
+<!--- start 15 lines (1)--->
+<div className="grid-container2">
+  <div id="lines" className="line1"></div>
+  
+  <div id="blue" className="line2"></div>
+  
+  <div id="lines" className="line3"></div>  
+  
+  <div id="lines" className="line4"></div>
+  
+  <div id="lines" className="line5"></div>
+    
+  <div id="lines" className="line6"></div>
+  
+  <div id="mid" className="line7"></div>  
+  
+  <div id="mid" className="line8"></div>
+  
+  <div id="mid" className="line9"></div>
+  
+  <div id="lines" className="line10"></div>
+  
+  <div id="lines" className="line11"></div>
+    
+  <div id="lines" className="line12"></div>
+  
+  <div id="lines" className="line13"></div>  
+  
+  <div id="lines" className="line14"></div>
+  
+  <div id="lines" className="line15"></div>
+</div>
+
+<!-- 15 lines (2) -->
+<div className="grid-container2">
+  <div id="lines" className="line1"></div>
+  
+  <div id="blue" className="line2"></div>
+  
+  <div id="blue" className="line3"></div>  
+  
+  <div id="blue" className="line4"></div>
+  
+  <div id="blue" className="line5"></div>
+    
+  <div id="blue" className="line6"></div>
+  
+  <div id="mid" className="line7"></div>  
+  
+  <div id="mid" className="line8"></div>
+  
+  <div id="mid" className="line9"></div>
+  
+  <div id="green" className="line10"></div>
+  
+  <div id="green" className="line11"></div>
+    
+  <div id="green" className="line12"></div>
+  
+  <div id="green" className="line13"></div>  
+  
+  <div id="green" className="line14"></div>
+  
+  <div id="lines" className="line15"></div>
+</div>
+
+<!-- 15 lines (3)-->
+<div className="grid-container2">
+  <div id="lines" className="line1"></div>
+  
+  <div id="lines" className="line2"></div>
+  
+  <div id="lines" className="line3"></div>  
+  
+  <div id="lines" className="line4"></div>
+  
+  <div id="lines" className="line5"></div>
+    
+  <div id="lines" className="line6"></div>
+  
+  <div id="mid" className="line7"></div>  
+  
+  <div id="mid" className="line8"></div>
+  
+  <div id="mid" className="line9"></div>
+  
+  <div id="lines" className="line10"></div>
+  
+  <div id="lines" className="line11"></div>
+    
+  <div id="lines" className="line12"></div>
+  
+  <div id="lines" className="line13"></div>  
+  
+  <div id="green" className="line14"></div>
+  
+  <div id="lines" className="line15"></div>
+</div>
+
+<!-- yellow & green --->
+<div className="grid-container">
+  <div id="first" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="yellow" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="fifth" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="first" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="yellow" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="fifth" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="first" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="yellow" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="fifth" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="first" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="yellow" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="fifth" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="first" className="item1"></div>
+  
+  <div id="yellow" className="item2"></div>
+  
+  <div id="yellow" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="fifth" className="item5"></div>
+</div>
+
+<div className="grid-container">
+  <div id="first" className="item1"></div>
+  
+  <div className="item2"></div>
+  
+  <div id="white" className="item3"></div>  
+  
+  <div className="item4"></div>
+  
+  <div id="fifth" className="item5"></div>
+</div>
+ <p id="dev">
+     Developed by <a href="https://eseodiase.website/">Ese Odiase</a>
+ </p>
+
 </section>;
     };
 
