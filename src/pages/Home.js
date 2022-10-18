61bd07b2ca7e33768a9334113ef27fb60036176a
Ese Odiase <50231281+eseodiase@users.noreply.github.com>
2022-10-18 02:57:48 +0100

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,7 @@
 import React from "react";
 const Home = () => {
      return <section>
-
-<section className="header">
-   <div className="search">
-      <form>
-      <h3 id="title">247 Lyrics Finder</h3>
-      <label>Song name:</label>
-      <input type="search" id="search-song" value="hello"/>
-      <br/>
-      <label>Artist name:</label>
-    <input type="search" id="search-artist" value="adele"/>
-    <p className="key"> Hit "Enter key" or tap "Search icon" on your keyboard/keypad to search.</p>
-    </form>
-    </div>
-</section>
-
-<section className="main">
-<section className="keyboard">
-    <div className="iframeContainer">
-    <h3>Song Lyrics</h3>
-    <p id="lyrics">Loading...</p>
-    </div>
-    </section>
-</section>
+<h1> Ludo Board </p>
 
 </section>;
     };
